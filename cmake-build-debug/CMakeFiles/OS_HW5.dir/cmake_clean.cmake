a487cd9d430b7f1a8a6357aa65025c88cdb4abcc
crossingfingers <ofekbransky@mail.tau.ac.il>
2021-06-09 16:51:54 +0300

    initial Code

---
+++ cmake-build-debug/CMakeFiles/OS_HW5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_HW5.dir/main.c.obj"
+  "OS_HW5.exe"
+  "OS_HW5.exe.manifest"
+  "OS_HW5.pdb"
+  "libOS_HW5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS_HW5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
